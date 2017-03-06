4893d8dffe74dce33a9ca981de531c13fa656a95
Necdet Can Ateşman <c.atesman@gmx.net>
2017-03-06 16:36:19 +0100

    New: Use unit testing repository

---
+++ test.conf.js
@@ -0,0 +1,9 @@
+var testConf = {
+    'router': 'local',
+    'init': '0.1.0',
+    'oop': '0.5.0'
+};
+
+if (typeof module != 'undefined') {
+    module.exports = testConf;
+}
--- test.js
+++ test.js
@@ -1,54 +1,10 @@
-if (typeof loadScore === 'undefined') {
+/* global loadScore:true, expect:true, describe, it, before, after, HTMLDivElement */
 
-    var loadScore = function loadScore(modules, callback) {
-        var fs = require('fs'),
-            request = require('sync-request'),
-            vm = require('vm');
-        if (typeof modules === 'function') {
-            callback = modules;
-            modules = [];
-        } else if (!modules) {
-            modules = [];
-        }
-        var loaded = {};
-        var customRequire = function(module) {
-            if (loaded[module]) {
-                return loaded[module];
-            }
-            var script, url, name = module.substring('score.'.length);
-            if (testConf[name] === 'local') {
-                script = fs.readFileSync(__dirname + '/../' + name.replace('.', '/') + '.js', {encoding: 'UTF-8'});
-            } else if (testConf[name]) {
-                url = 'https://raw.githubusercontent.com/score-framework/js.' + name + '/' + testConf[name] + '/' + name + '.js';
-            } else {
-                url = 'https://raw.githubusercontent.com/score-framework/js.' + name + '/master/' + name + '.js';
-            }
-            if (url) {
-                if (!loadScore.cache[url]) {
-                    loadScore.cache[url] = request('GET', url).getBody('utf8');
-                }
-                script = loadScore.cache[url];
-            }
-            var sandbox = vm.createContext({require: customRequire, module: {exports: {}}});
-            vm.runInContext(script, sandbox, module + '.js');
-            loaded[module] = sandbox.module.exports;
-            return loaded[module];
-        };
-        var score = customRequire('score.init');
-        for (var i = 0; i < modules.length; i++) {
-            customRequire('score.' + modules[i]);
-        }
-        callback(score);
-    };
-
-    loadScore.cache = {};
-
-    var expect = require('expect.js');
+if (typeof loadScore == 'undefined') {
+    var tmp = require('./node.js');
+    loadScore = tmp.loadScore;
+    expect = tmp.expect;
 }
-
-var testConf = {
-    'router': 'local'
-};
 
 describe('score.router', function() {
 
